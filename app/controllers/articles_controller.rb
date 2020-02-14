a4346ae8af81ec816912caf34f87118e7a373a0a
shahbaz <shahbazrajput3@gmail.com>
2020-02-14 09:51:32 +0500

    Destroy and making partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def new
     @article = Article.new
   end
@@ -37,6 +37,14 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash[:notice] = "Article was deleted successfuly"
+    redirect_to articles_path
   end
 
   private
